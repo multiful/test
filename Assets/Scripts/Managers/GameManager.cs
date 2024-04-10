58fa2e315d59b6cc84132d969d867117e5437d02
nilbace <101455214+nilbace@users.noreply.github.com>
2024-04-10 21:44:00 +0900

    더미데이터 추가2

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,18 +8,19 @@ public class GameManager : MonoBehaviour
 
     public static GameManager Inst { get { Init(); return s_instance; } }
 
+    NaniEngineManager _nani = new NaniEngineManager();
     //ResourceManager _resource = new ResourceManager();
     //UI_Manager _ui_manager = new UI_Manager();
     //SoundManager _sound = new SoundManager();
-    //DataManager _data = new DataManager();
+    DataManager _data = new DataManager();
     //NicknameManager _nickname = new NicknameManager();
     //SceneMManager _scene = new SceneMManager();
 
-
+    public static NaniEngineManager Nani { get { return Inst._nani; } }
     //public static ResourceManager Resource { get { return instance._resource; } }
     //public static UI_Manager UI { get { return instance._ui_manager; } }
     //public static SoundManager Sound { get { return instance._sound; } }
-    //public static DataManager Data { get { return instance._data; } }
+    public static DataManager Data { get { return Inst._data; } }
     //public static NicknameManager NickName { get { return instance._nickname; } }
     //public static SceneMManager Scene { get { return instance._scene; } }
 
@@ -43,7 +44,7 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(go);
             s_instance = go.GetComponent<GameManager>();
 
-            //s_instance._sound.Init();
+            s_instance._nani.Init();
             //s_instance._data.Init();
             //s_instance._scene.Init();
         }
